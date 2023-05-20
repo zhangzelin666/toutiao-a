2e39aa318a48ea89f220b5cb852214f963fc01c4
ZhangZe057271 <Zhang057271>
2023-05-20 16:13:37 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-//加载全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import './styles/index.less'
+// 加载vant
+
+Vue.use(Vant)
+//加载全局样式
 Vue.config.productionTip = false
 
 new Vue({
